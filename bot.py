f5267dab027726ee1fa0e1f9568fa3b5eb5a308c
lsw8075 <lsw8075@snu.ac.kr>
2016-05-06 13:21:09 +0900

    중대 버그 수정 + chemi 다인자, DT 지원

--- bot.py
+++ bot.py
@@ -61,7 +61,7 @@ def ondialog(channel, nick, text):
                 return '({:d}, {:d}, {:d})'.format(r,g,b)
             except:
                 deop(channel, nick, '이상한 값 넣지 말라구!')
-    elif text == '시리우스 피죤 물어!' or '시리우스 비둘기 물어!':
+    elif text == '시리우스 피죤 물어!' or text == '시리우스 비둘기 물어!':
         time.sleep(0.5)
         return '구구!! 피죤 살려!! ㅠㅠ'
 
@@ -74,66 +74,7 @@ def oncalled(channel, nick, text):
     elif text == '밥 먹자':
         return '구구~! 마시쪙? 마시쪙!'
 
-
-elements = '. H He Li Be B C N O F Ne Na Mg Al Si P S Cl Ar K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I Xe Cs Ba La Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu  Hf Ta W Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn Fr Ra Ac Th Pa U Np Pu Am Cm Bk Cf Es Fm Md No Lr Rf Db Sg Bh Hs Mt Ds Rg Cn Fl Lv'
-bonelem = elements.split(' ')
-elemlist = elements.upper().split(' ')
-
-def whichelem(x):
-    try:
-        return elemlist.index(x)
-    except:
-        return 0;
-    
-def chemi(word):
-
-    if word == '':
-        return '불가능'
-
-    word = str(word).upper()
-
-    whatelem = [128]
-    whatelem.append(whichelem(word[0]))
-    whereelem = [3, 1]
-
-    if len(word) == 1:
-        if whatelem[1] == 0:
-            return '불가능'
-        else:
-            return bonelem[whatelem[1]]
-
-    # word[0:i] 가 되는지 확인
-    for i in range(2, len(word)+1):
-        x = whichelem(word[i-1])
-        y = whichelem(word[i-2:i])
-        if whatelem[i-2] != 0 and y != 0:
-            whereelem.append(2)
-            whatelem.append(y)
-        elif whatelem[i-1] != 0 and x != 0:
-            whereelem.append(1)
-            whatelem.append(x)
-        else:
-            whereelem.append(0)
-            whatelem.append(0)
-    
-    i = len(word)
-
-    if whatelem[i] == 0:
-        return '불가능'
-
-    bonlist = []
-    while whereelem[i] != 3:
-        bonlist.append(whatelem[i])
-        i = i - whereelem[i]
-
-    bonlist.reverse()
-
-    res = ''
-    for i in bonlist:
-        res = res + bonelem[i]
-
-    return res
-        
+from chemi import tochemi       
 
 # 명령어
 def oncommand(channel, nick, command, args):
@@ -148,8 +89,11 @@ def oncommand(channel, nick, command, args):
             return res
         except:
             deop(channel, nick, '이상한 값 넣지 말라구!')
-    if command == '!chemi' and len(args) == 1:
-        return '분석 결과: ' + chemi(args[0])
+    if command == '!chemi' and len(args) > 0:
+        res = '분석 결과: '
+        for arg in args:
+            res = res + tochemi(arg) + ' ' 
+        return res
 
 # 여기부터 엔진 부분.
 
@@ -301,7 +245,7 @@ def run():
 
         elif msg.msgType == "PRIVMSG":
             if str(msg.msg)[0] == '!': # 명령어 처리
-                arg = str(msg.msg).split(' ')
+                arg = str(msg.msg).split()
                 txt = oncommand(msg.channel, msg.nick, arg[0], arg[1:])
                 if txt != None:
                     send_msg(msg.channel, txt)
---
+++ chemi.py
@@ -0,0 +1,60 @@
+
+elements = '. H He Li Be B C N O F Ne Na Mg Al Si P S Cl Ar K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I Xe Cs Ba La Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu Hf Ta W Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn Fr Ra Ac Th Pa U Np Pu Am Cm Bk Cf Es Fm Md No Lr Rf Db Sg Bh Hs Mt Ds Rg Cn Fl Lv D T'
+bonelem = elements.split(' ')
+elemlist = elements.upper().split(' ')
+
+def whichelem(x):
+    try:
+        return elemlist.index(x)
+    except:
+        return 0;
+    
+def tochemi(word):
+
+    if word == '':
+        return '불가능'
+
+    word = str(word).upper()
+
+    whatelem = [128]
+    whatelem.append(whichelem(word[0]))
+    whereelem = [3, 1]
+
+    if len(word) == 1:
+        if whatelem[1] == 0:
+            return '불가능'
+        else:
+            return bonelem[whatelem[1]]
+
+    # word[0:i] 가 되는지 확인
+    for i in range(2, len(word)+1):
+        x = whichelem(word[i-1])
+        y = whichelem(word[i-2:i])
+        if whatelem[i-2] != 0 and y != 0:
+            whereelem.append(2)
+            whatelem.append(y)
+        elif whatelem[i-1] != 0 and x != 0:
+            whereelem.append(1)
+            whatelem.append(x)
+        else:
+            whereelem.append(0)
+            whatelem.append(0)
+    
+    i = len(word)
+
+    if whatelem[i] == 0:
+        return '불가능'
+
+    bonlist = []
+    while whereelem[i] != 3:
+        bonlist.append(whatelem[i])
+        i = i - whereelem[i]
+
+    bonlist.reverse()
+
+    res = ''
+    for i in bonlist:
+        res = res + bonelem[i]
+
+    return res
+
